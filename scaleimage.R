5bf46190c23cde5dc60abb70d6d69d8d58255370
Dael <dael.separa@gmail.com>
2015-08-24 11:26:07 +0800

    add function to increase image background without any other processing

--- scaleimage.R
+++ scaleimage.R
@@ -1,3 +1,4 @@
+# scale image by a factor of sf. can only handle natural numbers for now
 magnify_image <- function(im, sf) {
 	
 	size = dim(im)
@@ -13,4 +14,12 @@ magnify_image <- function(im, sf) {
 	}
 	
 	return(magnifiedImage)
+}
+
+# increase image background
+increase_background <- function(im, sf) {
+	size = dim(im)
+	newImage = array(0,size*sf)
+	newImage[1:size[1],1:size[2]] = im
+	return(newImage)
 }
