f21fc859b7284b1a0c14a27c70cc463ce29309a2
Dael <dael.separa@gmail.com>
2015-08-25 10:18:33 +0800

    added acknowledgement to hu_moments()

--- hu_moments.R
+++ hu_moments.R
@@ -1,32 +1,32 @@
-hu_moments <- function(f) {
+# adapted from http://limitless-thoughts.blogspot.com/2011/05/hus-seven-moments-invariant-matlab-code.html
 
+hu_moments <- function(f) {
 	size = dim(f)
-	
-	x_ = as.vector(outer(array(0,size[1]),array(1:size[2]),FUN='+'))
-	y_ = as.vector(outer(array(1:size[1]),array(0,size[2]),FUN='+'))
+	x = as.vector(outer(array(0,size[1]),array(1:size[2]),FUN='+'))
+	y = as.vector(outer(array(1:size[1]),array(0,size[2]),FUN='+'))
 	f_ = as.vector(f)
 
 	m00 = sum(f_)
-	m10 = sum(x_*f_)
-	m01 = sum(y_* f_)
-	m11 = sum(x_* y_*f_)
-	m20 = sum(x_^2 * f_)
-	m02 = sum(y_^2 * f_)
-	m30 = sum(x_^3 * f_)
-	m03 = sum(y_^3 * f_)
-	m12 = sum(x_ * y_^2 * f_)
-	m21 = sum(x_^2 * y_* f_)
+	m10 = sum(x*f_)
+	m01 = sum(y*f_)
+	m11 = sum(x*y*f_)
+	m20 = sum(x^2*f_)
+	m02 = sum(y^2*f_)
+	m30 = sum(x^3*f_)
+	m03 = sum(y^3*f_)
+	m12 = sum(x*y^2*f_)
+	m21 = sum(x^2*y*f_)
 	
-	xbar = m10 / m00
-	ybar = m01 / m00
+	xbar = m10/m00
+	ybar = m01/m00
 
-	eta11 = (m11 - ybar*m10) / m00^2
-	eta20 = (m20 - xbar*m10) / m00^2
-	eta02 = (m02 - ybar*m01) / m00^2
-	eta30 = (m30 - 3 * xbar * m20 + 2 * xbar^2 * m10) / m00^2.5
-	eta03 = (m03 - 3 * ybar * m02 + 2 * ybar^2 * m01) / m00^2.5
-	eta21 = (m21 - 2 * xbar * m11 - ybar * m20 + 2 * xbar^2 * m01) / m00^2.5
-	eta12 = (m12 - 2 * ybar * m11 - xbar * m02 + 2 * ybar^2 * m10) / m00^2.5
+	eta11 = (m11-ybar*m10)/m00^2
+	eta20 = (m20-xbar*m10)/m00^2
+	eta02 = (m02-ybar*m01)/m00^2
+	eta30 = (m30-3*xbar*m20+2*xbar^2*m10)/m00^2.5
+	eta03 = (m03-3*ybar*m02+2*ybar^2*m01)/m00^2.5
+	eta21 = (m21-2*xbar*m11-ybar*m20+2*xbar^2*m01)/m00^2.5
+	eta12 = (m12-2*ybar*m11-xbar*m02+2*ybar^2*m10)/m00^2.5
 	
 	phi = array(0,7)
 	phi[1] = eta20+eta02
