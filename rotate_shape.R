07f17de77927e5fbb4d8d28843165d5f6cc720e4
Dael <dael.separa@gmail.com>
2015-09-01 01:19:19 +0800

    Add function to rotate shape

---
+++ rotate_shape.R
@@ -0,0 +1,16 @@
+rotate_shape <- function(vertices, degree_angle, origin_x, origin_y) {
+  
+  # assumes vertices is a 2-column vector
+  rotated_ = array(0,dim(vertices))
+
+  # center shape about origin
+  theta_ = degree_angle*pi/180.0
+  x_ = vertices[,1] - mean(vertices[,1])
+  y_ = vertices[,2] - mean(vertices[,2])
+  
+  # perform rotation
+  rotated[,1] = x_*cos(theta_) - y_*sin(theta_)
+  rotated[,2] = x_*sin(theta_) - y_*cos(theta_)
+  
+  return(rotated)
+}
