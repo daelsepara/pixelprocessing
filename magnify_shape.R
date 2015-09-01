df2b9b9141171a8a39e9a17e01965cd934f0a802
Dael <dael.separa@gmail.com>
2015-09-01 15:17:17 +0800

    simplified magnify_shape

--- magnify_shape.R
+++ magnify_shape.R
@@ -16,8 +16,8 @@ magnify_shape <- function(f, sf) {
 		for (sf_ in 1:sf) {
 			
 			# stretch shape
-			f_[sf*(i-1)+sf_, 1] = origin_x +(f[i %% size[1] + 1, 1]-f[i, 1])*(sf-1*(sf-1))
-			f_[sf*(i-1)+sf_, 2] = origin_y +(f[i %% size[1] + 1, 2]-f[i, 2])*(sf-1*(sf-1))
+			f_[sf*(i-1)+sf_, 1] = origin_x +(f[i %% size[1] + 1, 1]-f[i, 1])
+			f_[sf*(i-1)+sf_, 2] = origin_y +(f[i %% size[1] + 1, 2]-f[i, 2])
 			f_[sf*(i-1)+sf_, 3] = f[i, 3]
 			
 			# set new origin
--- rotate_shape.R
+++ rotate_shape.R
@@ -1,16 +1,17 @@
-rotate_shape <- function(vertices, degree_angle, origin_x, origin_y) {
+rotate_shape <- function(f, degree_angle, origin_x, origin_y) {
   
-  # assumes vertices is a 2-column vector
-  rotated_ = array(0,dim(vertices))
+  # assumes vertices is a 3-column vector (x. y, gray level)
+  rotated_ = array(0,dim(f))
 
   # center shape about origin
   theta_ = degree_angle*pi/180.0
-  x_ = vertices[,1] - origin_x
-  y_ = vertices[,2] - origin_y
+  x_ = f[,1] - origin_x
+  y_ = f[,2] - origin_y
   
   # perform rotation
   rotated_[,1] = x_*cos(theta_) - y_*sin(theta_)
   rotated_[,2] = x_*sin(theta_) + y_*cos(theta_)
+  rotated_[,3] = f[,3]
   
   return(rotated_)
 }
