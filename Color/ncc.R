5c98ca3649dbf6778a38528b082b77ed125100c4
Dael <dael.separa@gmail.com>
2015-09-12 11:48:43 +0800

    histogram backpropagation

--- Color/ncc.R
+++ Color/ncc.R
@@ -15,3 +15,43 @@ ncc <- function(rgb_) {
 	
 	return(rg_)
 }
+
+color_hist <- function(ncc_, bins) {
+
+	rint = round(ncc_[, , 1]*(bins-1) + 1)
+	gint = round(ncc_[, , 2]*(bins-1) + 1)
+ 
+	cols_ = as.vector(gint) + (as.vector(rint)-1)*bins
+	hist_ = array(0, c(bins,bins))
+	
+	for (row_ in 1:bins) {
+		for(col_ in 1:(bins - row_ + 1)) {
+			hist_[row_, col_] = length(which(cols_ == (col_ + (row_-1)*bins)));
+		}
+	}
+	
+	return(hist_)
+}
+
+backpropagate_hist <- function(img_, hist_, bins) {
+
+	size = dim(img_)
+	
+	rows_ = size[1];
+	cols_ = size[2];
+ 
+	backproj = array(0,c(rows_,cols_));
+	
+	for (i in 1:rows_) {
+		for (j in 1:cols_) {
+			
+			r_new = round(img_[i, j, 1]*(bins-1) + 1)
+			g_new = round(img_[i, j, 2]*(bins-1) + 1)
+			
+			backproj[i, j] = hist_[r_new, g_new]
+      
+		}
+	}
+	
+	return(backproj)
+}
