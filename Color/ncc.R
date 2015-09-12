c07db17d2eccb703cacce61f29828d8abfc0ccf7
Dael <dael.separa@gmail.com>
2015-09-12 12:05:15 +0800

    optimize computation of NCC

--- Color/ncc.R
+++ Color/ncc.R
@@ -1,15 +1,16 @@
 ncc <- function(rgb_) {
+	
 	size = dim(rgb_)
 	
-	rg_ = array(0, c(size[1],size[2],2))
+	rg_ = array(0, c(size[1], size[2], 2))
 	
 	for (y in 1:size[1]) {
 		for (x in 1:size[2]) {
 			
-			I_ = sum(rgb_[y, x,])
+			i_ = sum(rgb_[y, x,])
 			
-			rg_[y, x, 1] = rgb_[y, x, 1]/I_
-			rg_[y, x, 2] = rgb_[y, x, 2]/I_
+			rg_[y, x, 1] = rgb_[y, x, 1]/i_
+			rg_[y, x, 2] = rgb_[y, x, 2]/i_
 		}
 	}
 	
@@ -18,40 +19,40 @@ ncc <- function(rgb_) {
 
 color_hist <- function(ncc_, bins) {
 
-	rint = round(ncc_[, , 1]*(bins-1) + 1)
-	gint = round(ncc_[, , 2]*(bins-1) + 1)
+	rint = round(ncc_[, , 1]*(bins - 1) + 1)
+	gint = round(ncc_[, , 2]*(bins - 1) + 1)
  
-	cols_ = as.vector(gint) + (as.vector(rint)-1)*bins
-	hist_ = array(0, c(bins,bins))
+	cols_ = as.vector(gint) + (as.vector(rint) - 1)*bins
+	
+	hist_ = array(0, c(bins, bins))
 	
 	for (row_ in 1:bins) {
 		for(col_ in 1:(bins - row_ + 1)) {
-			hist_[row_, col_] = length(which(cols_ == (col_ + (row_-1)*bins)));
+		
+			hist_[row_, col_] = length(which(cols_ == (col_ + (row_-1)*bins)))
+			
 		}
 	}
 	
 	return(hist_)
 }
 
-backpropagate_hist <- function(img_, hist_, bins) {
+backpropagate_hist <- function(ncc_, hist_, bins) {
 
-	size = dim(img_)
+	size = dim(ncc_)
 	
-	rows_ = size[1];
-	cols_ = size[2];
- 
-	backproj = array(0,c(rows_,cols_));
+	backp_ = array(0, c(size[1], size[2]))
 	
-	for (i in 1:rows_) {
-		for (j in 1:cols_) {
+	for (y in 1:size[1]) {
+		for (x in 1:size[2]) {
 			
-			r_new = round(img_[i, j, 1]*(bins-1) + 1)
-			g_new = round(img_[i, j, 2]*(bins-1) + 1)
+			r_ = round(ncc_[y, x, 1]*(bins - 1) + 1)
+			g_ = round(ncc_[y, x, 2]*(bins - 1) + 1)
 			
-			backproj[i, j] = hist_[r_new, g_new]
+			backp_[y, x] = hist_[r_, g_]
       
 		}
 	}
 	
-	return(backproj)
+	return(backp_)
 }
