dcc0bab3970cd2a2689cd5fac4c7ee48cdeefb28
Dael <dael.separa@gmail.com>
2015-10-15 21:08:36 +0800

    Add comments to PCA image compression

--- Component/pca_compress.R
+++ Component/pca_compress.R
@@ -1,14 +1,21 @@
 pca_compress <- function(img_, k_, type_ = 'pca') {
 
+	# solve eigenvalue problem
 	eigen_ob = eigen(cov(img_ - rowMeans(img_)))
 	eigen_vectors = eigen_ob$vectors
-	ev = eigen_vectors[,1:k_]
-	
+
+	# return eigenvalues
 	if (type_ == 'values') {
 		return(eigen_ob$values)
 	}
 	
+	# use desired number of PCA components (from eigenvectors)
+	ev = eigen_vectors[, 1:k_]
+	
+	# project image using new basis functions
 	y = t(ev) %*% t(img_)
+	
+	# reconstruct image
 	x = t(ev %*% y) + rowMeans(img_)
 	
 	return(x)
