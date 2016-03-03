95457f27156375af8206a19bd3caca79db04897e
Dael <dael.separa@gmail.com>
2016-03-03 15:21:25 +0800

    add nnet_optimize which uses an advanced conjugate gradient descent method (fmincg). Add regularization option for nnet_optimize when using nnet_backprop

--- NeuralNetwork/AdvancedNeuralNetwork.R
+++ NeuralNetwork/AdvancedNeuralNetwork.R
@@ -29,14 +29,11 @@ nnet_forward <- function(training_set, w_ji, w_kj) {
 }
 
 # backward propagation
-nnet_backprop <- function(training_set, y_k, z_2, a_2, w_ji, w_kj, y_matrix) {
+nnet_backprop <- function(training_set, y_k, z_2, a_2, w_ji, w_kj, y_matrix, lambda = 0) {
 	
 	# add bias column
 	x = cbind(array(1, c(nrow(training_set), 1)), training_set)
 	m = nrow(x)
-	
-	# cost function
-	cost = sum(-y_matrix * log(y_k) - (1 - y_matrix)*log(1 - y_k))/m
 	
 	# compute intermediate delta values per layer
 	d3 = y_k - y_matrix
@@ -45,8 +42,45 @@ nnet_backprop <- function(training_set, y_k, z_2, a_2, w_ji, w_kj, y_matrix) {
 	# compute gradient
 	dWji = (t(d2) %*% x)/m
 	dWkj = (t(d3) %*% a_2)/m
+
+	# cost function
+	cost = sum(-y_matrix * log(y_k) - (1 - y_matrix)*log(1 - y_k))/m
 	
+	# regularization on lambda != 0
+	if (lambda != 0) {
+	
+		rWji = w_ji
+		rWkj = w_kj
+		
+		rWji[, 1] = array(0, nrow(w_ji))
+		rWkj[, 1] = array(0, nrow(w_kj))
+		
+		cost = cost + lambda*(sum(rWji*rWji) + sum(rWkj*rWkj))/(2*m)
+		
+		dWji = dWji + lambda*rWji/m
+		dWkj = dWkj + lambda*rWkj/m
+	}
+
 	return(list('dWkj' = dWkj, 'dWji' = dWji, 'Error' = cost))	
+}
+
+nnet_cost <- function(X, P1, P2, P3 , P4, P5, P6) {
+
+	# P1 training_set
+	# P2 y_matrix
+	# P3 number of inputs
+	# P4 hidden layer units
+	# P5 number of labels
+
+	w_ji = array(X[1:(P4 * (P3 + 1))], c(P4, P3 + 1))
+	w_kj = array(X[(1 + (P4 * (P3 + 1))):length(X)], c(P5, P4 + 1))
+
+	forward = nnet_forward(P1, w_ji, w_kj)
+	result = nnet_backprop(P1, forward$y_k, forward$z_2, forward$a_2, w_ji, w_kj, P2, P6)
+
+	grad = c(as.vector(result$dWji), as.vector(result$dWkj))
+
+	return(list('J' = result$Error, 'grad' = grad))
 }
 
 # network training
@@ -84,9 +118,9 @@ nnet_train <-function(maxiter = 100, learning_rate = 0.1, tol = 10^(-3), trainin
 	
 	while (iter < maxiter && Error > tol) {
 	
-		# for training, perform forward and backpropagation each iteration
+		# for training, perform forward and backpropagation each iteration, no regularization
 		forward = nnet_forward(training_set, w_ji, w_kj)
-		backward = nnet_backprop(training_set, forward$y_k, forward$z_2, forward$a_2, w_ji, w_kj, y_matrix)
+		backward = nnet_backprop(training_set, forward$y_k, forward$z_2, forward$a_2, w_ji, w_kj, y_matrix, 0)
 		
 		# update weights (using learning rate and gradient descent)
 		w_ji = w_ji - learning_rate * backward$dWji
@@ -107,6 +141,54 @@ nnet_train <-function(maxiter = 100, learning_rate = 0.1, tol = 10^(-3), trainin
 	prediction = nnet_predict(training_set, w_ji, w_kj)
 	
 	return(list('y_k' = y_k, 'Error' = Error, 'iterations' = iter, 'w_kj' = w_kj, 'w_ji' = w_ji, 'prediction' = prediction))
+}
+
+# network optimization
+nnet_optimize <-function(maxiter = 100, training_set = array(0) , output = array(0), hidden_units = 0, num_labels = 1, min_max = 1, isGaussian = FALSE, lambda = 0) {
+
+	# For multi-classification problem, format expected output
+	# i.e. matrix, each row corresponds to a training pattern.
+	# Each element in the row-vector is a 0 or 1 indicating whether
+	# or not it belongs to that particular class
+	if (num_labels > 1) {
+		eye_matrix = diag(num_labels)
+		y_matrix = eye_matrix[output, ]
+	} else {
+		# binary classification
+		y_matrix = output
+	}
+	
+	# determine network dimensions from user input
+	j = hidden_units
+	inputs = ncol(training_set)
+  
+	# intialize interconnection weights with random values (-min_max, min_max) or Gaussian (mean = 0, sd = min_max)
+	if (!isGaussian) {
+		w_ji = array(runif(n = j * (inputs + 1), min = -min_max, max = min_max), c(j, inputs + 1))
+		w_kj = array(runif(n = num_labels * (j + 1), min = -min_max, max = min_max), c(num_labels, j + 1))
+	} else {
+		w_ji = array(rnorm(n = j * (inputs + 1), mean = 0, sd = abs(min_max)), c(j, inputs + 1))
+		w_kj = array(rnorm(n = num_labels * (j + 1), mean = 0, sd = abs(min_max)), c(num_labels, j + 1))
+	}
+  
+	Error = 1.0
+	
+	y_k = numeric(0)
+
+	initialWeights = c(as.vector(w_ji), as.vector(w_kj))
+	optimizationResult = fmincg(nnet_cost, initialWeights, maxiter, training_set, y_matrix, inputs, j, num_labels, lambda)
+	
+	w_ji = array(optimizationResult$X[1:(j * (inputs + 1))], c(j, inputs + 1))
+	w_kj = array(optimizationResult$X[(1 + (j * (inputs + 1))):length(optimizationResult$X)], c(num_labels, j + 1))
+		
+	# save current performance
+	Error = optimizationResult$cost
+	y_k = nnet_forward(training_set, w_ji, w_kj)
+    
+	# add prediction
+	prediction = nnet_predict(training_set, w_ji, w_kj)
+	
+	return(list('y_k' = y_k, 'Error' = Error, 'w_kj' = w_kj, 'w_ji' = w_ji, 'prediction' = prediction))
 }
 
 # predict using neural network parameters (multi-class classification)
---
+++ NeuralNetwork/fmincg.R
@@ -0,0 +1,288 @@
+fmincg<-function(f, X, MaxIter = 100, P1, P2, P3, P4, P5, P6) {
+# Minimize a continuous differentialble multivariate function. Starting point
+# is given by "X" (D by 1), and the function named in the string "f", must
+# return a function value and a vector of partial derivatives. The Polack-
+# Ribiere flavour of conjugate gradients is used to compute search directions,
+# and a line search using quadratic and cubic polynomial approximations and the
+# Wolfe-Powell stopping criteria is used together with the slope ratio method
+# for guessing initial step sizes. Additionally a bunch of checks are made to
+# make sure that exploration is taking place and that extrapolation will not
+# be unboundedly large. The "length" gives the length of the run: if it is
+# positive, it gives the maximum number of line searches, if negative its
+# absolute gives the maximum allowed number of function evaluations. You can
+# (optionally) give "length" a second component, which will indicate the
+# reduction in function value to be expected in the first line-search (defaults
+# to 1.0). The function returns when either its length is up, or if no further
+# progress can be made (ie, we are at a minimum, or so close that due to
+# numerical problems, we cannot get any closer). If the function terminates
+# within a few iterations, it could be an indication that the function value
+# and derivatives are not consistent (ie, there may be a bug in the
+# implementation of your "f" function). The function returns the found
+# solution "X", a vector of function values "fX" indicating the progress made
+# and "i" the number of iterations (line searches or function evaluations,
+# depending on the sign of "length") used.
+#
+# Usage: [X, fX, i] = fmincg(f, X, options, P1, P2, P3, P4, P5, P6)
+#
+# See also: checkgrad 
+#
+# Copyright (C) 2001 and 2002 by Carl Edward Rasmussen. Date 2002-02-13
+#
+#
+# (C) Copyright 1999, 2000 & 2001, Carl Edward Rasmussen
+# 
+# Permission is granted for anyone to copy, use, or modify these
+# programs and accompanying documents for purposes of research or
+# education, provided this copyright notice is retained, and note is
+# made of any changes that have been made.
+# 
+# These programs and documents are distributed without any warranty,
+# express or implied.  As the programs were written for research
+# purposes only, they have not been tested to the degree that would be
+# advisable in any important application.  All use of these programs is
+# entirely at the user's own risk.
+#
+# [ml-class] Changes Made:
+# 1) Function name and argument specifications
+# 2) Output display
+#
+# This conversion (to R) by:  SD Separa (2016)
+
+	realmin = .Machine$double.xmin
+
+	red=1
+	S='Iteration '
+
+	RHO = 0.01		# a bunch of constants for line searches
+	SIG = 0.5		# RHO and SIG are the constants in the Wolfe-Powell conditions
+	INT = 0.1		# don't reevaluate within 0.1 of the limit of the current bracket
+	EXT = 3.0		# extrapolate maximum 3 times the current bracket
+	MAX = 20		# max 20 function evaluations per line search
+	RATIO = 100		# maximum allowed slope ratio
+
+	i = 0 			# zero the run length counter
+	ls_failed = 0	# no previous line search has failed
+	fX = numeric(0)
+
+	# get function value and gradient
+	eval_ = f(X, P1, P2, P3 , P4, P5, P6)
+	f1 = eval_$J
+	df1 = eval_$grad
+
+	i = i + (MaxIter > 0) 			# count epochs?!
+	s = -df1            # search direction is steepest
+	d1 = t(-s) %*% s    # this is the slope
+	z1 = red/(1-d1)     # initial step is red/(|s|+1)
+
+	while (i < abs(MaxIter)) {
+	  
+		# count iterations?!
+		i = i + (MaxIter > 0)
+
+		# make a copy of current values
+		X0 = X
+		f0 = f1
+		df0 = df1
+
+		# begin line search
+		X = X + z1 %*% s
+	  
+		eval_ = f(X, P1, P2, P3 , P4, P5, P6)                                 
+		f2 = eval_$J
+		df2 = eval_$grad
+		
+		# count epochs?!
+		i = i + (MaxIter > 0)
+		
+		# initialize point 3 equal to point 1
+		d2 = t(df2) %*% s
+		f3 = f1
+		d3 = d1
+		z3 = -z1
+		
+		if (MaxIter>0) {
+			M = MAX
+		} else {
+			M = min(MAX, -MaxIter-i)
+		}
+		
+		# initialize quantities
+		success = 0
+		limit = -1
+		
+		while(1) {
+			if (is.nan(f2) || is.nan(f1+z1*RHO*d1) || is.nan(d2) || is.nan(-SIG*d1) || is.nan(M)) {
+				break
+			}
+			
+			while (((f2 > f1+z1*RHO*d1) || (d2 > -SIG*d1)) && (M > 0)) {
+
+				# tighten the bracket
+				limit = z1
+				
+				if (f2 > f1) {
+					# quadratic fit
+					z2 = z3 - (0.5*d3*z3*z3)/(d3*z3+f2-f3)
+				} else {
+					# cubic fit
+					A = 6*(f2-f3)/z3+3*(d2+d3)
+					B = 3*(f3-f2)-z3*(d3+2*d2)
+					
+					# numerical error possible - ok!
+					z2 = suppressWarnings((sqrt(B*B-A*d2*z3*z3)-B)/A)
+				}
+			
+				if (is.nan(z2) || is.infinite(z2)) {
+					# if we had a numerical problem then bisect
+					z2 = z3/2
+				}
+				
+				# don't accept too close to limits
+				z2 = max(min(z2, INT*z3),(1-INT)*z3)
+				
+				# update the step
+				z1 = z1 + z2
+				X = X + z2 %*% s
+		  
+				eval_ = f(X, P1, P2, P3 , P4, P5, P6)
+				f2 = eval_$J
+				df2 = eval_$grad
+				
+				M = M - 1
+				
+				# count epochs?!
+				i = i + (MaxIter > 0)
+				
+				d2 = t(df2) %*% s
+				
+				# z3 is now relative to the location of z2
+				z3 = z3-z2;                    
+			}
+			
+			if (f2 > f1+z1*RHO*d1 || d2 > -SIG*d1) {
+				# this is a failure
+				break;
+			} else if (d2 > SIG*d1) {
+				# success
+				success = 1
+				break
+			} else if (M == 0) {
+				# failure
+				break
+			}	
+		
+			# make cubic extrapolation
+			A = 6*(f2-f3)/z3+3*(d2+d3)
+			B = 3*(f3-f2)-z3*(d3+2*d2)
+			
+			# num. error possible - ok!
+			z2 = suppressWarnings(-d2*z3*z3/(B+sqrt(B*B-A*d2*z3*z3)))
+			
+			# num prob or wrong sign?
+			if (is.nan(z2) || is.infinite(z2) || z2 < 0) {
+				# if we have no upper limit
+				if (limit < -0.5) {
+					# the extrapolate the maximum amount
+					z2 = z1 * (EXT-1)
+				} else {
+					# otherwise bisect
+					z2 = (limit-z1)/2
+				}
+			} else if ((limit > -0.5) && (z2+z1 > limit)) {
+				# extraplation beyond max?
+				z2 = (limit-z1)/2
+			} else if ((limit < -0.5) && (z2+z1 > z1*EXT)) {
+				# extrapolation beyond limit
+				# set to extrapolation limit
+				z2 = z1*(EXT-1.0)
+			} else  if (z2 < -z3*INT) {
+				z2 = -z3*INT
+			} else if ((limit > -0.5) && (z2 < (limit-z1)*(1.0-INT))) {
+				# too close to limit?
+				z2 = (limit-z1)*(1.0-INT);
+			}
+		
+			# set point 3 equal to point 2
+			f3 = f2
+			d3 = d2
+			z3 = -z2
+			
+			# update current estimates
+			z1 = z1 + z2
+			X = X + z2 %*% s
+			
+			eval_ = f(X, P1, P2, P3 , P4, P5, P6)
+			f2 = eval_$J
+			df2 = eval_$grad
+			M = M - 1
+			
+			# count epochs?!
+			i = i + (MaxIter > 0)
+			
+			d2 = t(df2)%*%s;
+			
+			# end of line search
+		}
+		
+		# if line search succeeded
+		if (success) {
+			
+			f1 = f2
+			fX = f1
+			
+			print(paste0(S, i, "| Cost: ", f1))
+			
+			# Polack-Ribiere direction
+			s = ((t(df2)%*%df2-t(df1)%*%df2)/(t(df1)%*%df1))*s - df2
+			
+			# swap derivatives
+			tmp = df1
+			df1 = df2
+			df2 = tmp
+			
+			d2 = t(df1)%*%s;
+
+			# new slope must be negative
+			if (d2 > 0)  {
+				# otherwise use steepest direction
+				s = -df1
+				d2 = -t(s)%*%s;    
+			}
+
+			# slope ratio but max RATIO
+			z1 = z1 * min(RATIO, d1/(d2-realmin))
+			d1 = d2
+			
+			# this line search did not fail
+			ls_failed = 0
+
+		} else {
+		
+			# restore point from before failed line search
+			X = X0
+			f1 = f0
+			df1 = df0  
+
+			# line search failed twice in a row
+			if (ls_failed || i > abs(MaxIter)) {
+				# or we ran out of time, so we give up
+				break
+			}
+
+			# swap derivatives
+			tmp = df1
+			df1 = df2
+			df2 = tmp
+			
+			# try steepest
+			s = -df1
+			d1 = -t(s) %*% s
+			z1 = 1/(1-d1)
+			
+			# this line search failed
+			ls_failed = 1
+		}
+	}
+	
+	return(list('X' = X, 'cost' = fX))
+}
